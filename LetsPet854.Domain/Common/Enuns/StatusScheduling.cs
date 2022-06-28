95b4fd0e20a55f866f1436aa1898677fb86ce797
Takeshi <takeshinishikawa@gmail.com>
2022-06-28 19:07:19 -0300

    Created class schedule, enums and messages

---
+++ LetsPet854.Domain/Common/Enuns/StatusScheduling.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsPet854.Domain.Common.Enuns
+{
+    public enum StatusScheduling
+    {
+        Livre = 0,
+        PréAgendado = 1,
+        Confirmado = 2,
+        Executado = 3,
+    }
+}
---
+++ LetsPet854.Domain/Common/Enuns/standardAnswer.cs
@@ -0,0 +1,10 @@
+﻿namespace LetsPet854.Domain.Common.Enuns
+{
+    public enum standardAnswer
+    {
+        Sim = 1,
+        Não = 2,
+        Sair = 3,
+        Retornar = 4,
+    }
+}
