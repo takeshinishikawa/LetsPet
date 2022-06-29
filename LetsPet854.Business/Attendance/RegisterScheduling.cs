3927b7ad39b8339c8019da388ff2c94b7ad7374a
carvalhosmari <mscarvall@gmail.com>
2022-06-29 17:30:06 -0300

    Criação de métodos provisórios apenas para validar a navegação do 'MenuAttendance'

---
+++ LetsPet854.Business/Attendance/RegisterScheduling.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPet854.Business.Attendance
+{
+    public class RegisterScheduling
+    {
+        public static void CreateSchedule()
+        {
+
+        }
+        public static void SchedulingRegister()
+        {
+
+        }
+    }
+}
