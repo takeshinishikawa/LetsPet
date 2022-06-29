499a522f62283a4e1cbb890bc8c4d87d03c59bc7
Matheus Alencastro <alencastromatheus@gmail.com>
2022-06-29 16:59:03 -0300

    Class validation (attendence) added

---
+++ LetsPet854.Business/Attendence/Validation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsPet854.Business.Attendence
+{
+    public class Validation
+    {
+        public static bool CheckAnswer(int answer) //LEMBRAR DE ALOCAR ESTE MÉTODO EM TODAS AS PERGUNTAS!!
+        {
+            while (answer != 1 || answer != 2 || answer != 3 || answer != 4)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
